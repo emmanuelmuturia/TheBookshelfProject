fb83eda80385d11a11a61e47d896f3bd08087212
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-06-25 13:05:00 +0300

    Set up and Configure ktlint, detekt, and spotless...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,60 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
+    alias(notation = libs.plugins.androidApplication).apply(apply = false)
+    alias(notation = libs.plugins.androidLibrary).apply(apply = false)
+    alias(notation = libs.plugins.kotlinAndroid).apply(apply = false)
+    alias(notation = libs.plugins.kotlinMultiplatform).apply(apply = false)
+    alias(notation = libs.plugins.compose.compiler).apply(apply = false)
+    alias(notation = libs.plugins.ktlint)
+    alias(notation = libs.plugins.detekt)
+    alias(notation = libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
+    ktlint {
+        verbose.set(true)
+        android.set(true)
+        filter {
+            exclude("**/generated/**")
+        }
+    }
+
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+            licenseHeaderFile(
+                "${project.rootDir}/spotless/copyright.kt",
+            )
+        }
+
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            // Look for the first line that does not have a block comment [assumed to be the license]...
+            licenseHeaderFile(
+                rootProject.file("spotless/copyright.kts"),
+                "(^(?![\\/ ]\\*).*$)",
+            )
+        }
+
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+        }
+
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            // Look for the first XML tag that isn't a comment [<!--] or the xml declaration [<?xml]...
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+        }
+    }
+}
+
+apply(plugin = rootProject.libs.plugins.detekt.get().pluginId)
+detekt {
+    parallel = true
 }
