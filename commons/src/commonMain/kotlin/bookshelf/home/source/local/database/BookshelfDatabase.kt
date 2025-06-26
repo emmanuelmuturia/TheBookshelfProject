5f1b6f825ae17c84dadf27ed979513040c3ce94e
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-06-26 18:29:17 +0300

    Set up the Room Database...

---
+++ commons/src/commonMain/kotlin/bookshelf/home/source/local/database/BookshelfDatabase.kt
@@ -0,0 +1,19 @@
+package bookshelf.home.source.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import bookshelf.home.source.local.dao.BookshelfDao
+import bookshelf.home.source.local.entity.BooksEntity
+import bookshelf.home.source.local.typeConverter.BookshelfTypeConverters
+
+@Database(
+    entities = [BooksEntity::class],
+    version = 1
+)
+@TypeConverters(value = [BookshelfTypeConverters::class])
+abstract class BookshelfDatabase : RoomDatabase() {
+
+    abstract fun bookshelfDao(): BookshelfDao
+
+}
